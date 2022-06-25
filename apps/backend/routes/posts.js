aa55a9af3e0289abb4a35edc333e19f59943ec2f
Patryk <byszek.patryk@gmail.com>
2022-06-25 02:25:16 +0200

    adding new posts to DB now possible

--- apps/backend/routes/posts.js
+++ apps/backend/routes/posts.js
@@ -15,6 +15,13 @@ router.route("/add").post((req, res) => {
   const text = req.body.text
   const hashtags = req.body.hashtags
   const createdBy = req.body.createdBy
+
+  const newPost = new Post({ title, text, hashtags, createdBy })
+
+  newPost
+    .save()
+    .then(() => res.json("New post has been created!"))
+    .catch((err) => res.status(400).res.json(`Error ${err}`))
 })
 
 module.exports = router
