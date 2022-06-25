28f10182ac6e81bc91cc2e9478c8d690afdaba6c
Patryk <byszek.patryk@gmail.com>
2022-06-25 02:10:42 +0200

    setting up post schema

---
+++ apps/backend/schemas/post.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const postSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    hashtags: { type: String, required: false },
+    createdBy: { type: String, required: true },
+    // img:
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Post = mongoose.model("Post", postSchema)
+
+module.exports = Post
