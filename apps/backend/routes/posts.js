45d7cd03cfce2d31b76031bcb09f8e22ad6e82e3
Patryk <byszek.patryk@gmail.com>
2022-06-26 02:05:06 +0200

    Posts can now be created, retrieved, upd & del

--- apps/backend/routes/posts.js
+++ apps/backend/routes/posts.js
@@ -7,7 +7,7 @@ router.route("/").get((req, res) => {
     .then((posts) => {
       res.json(posts)
     })
-    .catch((err) => res.status(400).json(`Error ${err}`))
+    .catch((err) => res.status(400).json(`Error ${err}.`))
 })
 
 router.route("/add").post((req, res) => {
@@ -21,7 +21,33 @@ router.route("/add").post((req, res) => {
   newPost
     .save()
     .then(() => res.json("New post has been created!"))
-    .catch((err) => res.status(400).res.json(`Error ${err}`))
+    .catch((err) => res.status(400).res.json(`Error ${err}.`))
+})
+
+router.route("/:id").get((req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json(`Error ${err}.`))
+})
+
+router.route("/:id").delete((req, res) => {
+  Post.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Post has been deleted."))
+    .catch((err) => res.status(400).json(`Error ${err}.`))
+})
+
+router.route("/update/:id").post((req, res) => {
+  Post.findById(req.params.id).then((post) => {
+    post.title = req.body.title
+    post.text = req.body.text
+    post.hashtags = req.body.hashtags
+    post.createdBy = req.body.createdBy
+
+    post
+      .save()
+      .then(() => res.json(`Post has been edited.`))
+      .catch((err) => res.status(400).json(`Error ${err}.`))
+  })
 })
 
 module.exports = router
