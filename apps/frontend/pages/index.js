3a389cf882f503d9f2aa7bc693b91e520cf823ce
Patryk <byszek.patryk@gmail.com>
2022-07-02 23:58:15 +0200

    Initial body to display list of posts

--- apps/frontend/pages/index.js
+++ apps/frontend/pages/index.js
@@ -1,15 +1,54 @@
 import Head from "next/head"
-import Image from "next/image"
 
-import { useGetPostsQuery } from "../redux/api/postsApiSlice"
+import {
+  useGetPostsQuery,
+  useCreatePostMutation,
+} from "../redux/api/postsApiSlice"
+
+import PostCard from "../components/PostCard"
+
+import { Button, Container, Input } from "@chakra-ui/react"
 
 const Home = () => {
-  const { data, error, isLoading } = useGetPostsQuery()
+  const { data, isLoading, isSuccess, isError, error } = useGetPostsQuery()
+  const [createPost] = useCreatePostMutation()
+
+  const postsList = data?.map((post) => <PostCard key={post._id} post={post} />)
+
+  const handleSubmit = (title, text, shortD, author) => {
+    const newPost = {
+      title: "app post!",
+      shortDescription: "This is a test after adding SD",
+      text: "Really really curious",
+      hashtags: ["test", "first-one", "hehe", "edited"],
+      createdBy: "Bysiu",
+    }
+
+    console.log(newPost)
+    createPost(newPost)
+  }
+
   return (
-    <div>
-      yo
-      {JSON.stringify(data)}
-    </div>
+    <Container>
+      <Head>
+        <title>Blog</title>
+        <meta name="description" content="Blog app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Button
+        onClick={() => {
+          handleSubmit()
+        }}
+      >
+        Add new post
+      </Button>
+
+      {isLoading ? "Loading" : null}
+      {isSuccess ? postsList : null}
+      {/* {isSuccess ? JSON.stringify(data) : null} */}
+      {isError ? JSON.stringify(error) : null}
+    </Container>
   )
 }
 
