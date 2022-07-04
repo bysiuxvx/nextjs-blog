ea806ba3c3eb850bac7a165ae44c29f81b1dd11a
Patryk <byszek.patryk@gmail.com>
2022-07-04 23:58:30 +0200

    Built image preview of selected file

--- apps/frontend/components/CreatePostForm/ImageThumbnail.js
+++ apps/frontend/components/CreatePostForm/ImageThumbnail.js
@@ -2,18 +2,14 @@ import { Image } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 
 const ImageThumbnail = ({ image }) => {
-  const [loading, setLoading] = useState(false)
   const [imgData, setImgData] = useState(null)
 
   useEffect(() => {
     if (!image) return
 
-    console.log("zmiana")
-    setLoading(true)
     let reader = new FileReader()
 
     reader.onloadend = () => {
-      setLoading(false)
       setImgData(reader.result)
     }
     reader.readAsDataURL(image)
@@ -30,7 +26,6 @@ const ImageThumbnail = ({ image }) => {
     // //   reader.readAsDataURL(e.target.files[0])
   }, [image])
 
-  if (loading) return <div>Loading...</div>
   if (imgData)
     return <Image src={imgData} alt="thumbnail" maxH={200} maxW={200} />
 }
