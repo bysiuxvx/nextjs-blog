e256309c76464107c808998c8722aad71bd6af9d
Patryk <byszek.patryk@gmail.com>
2022-07-10 21:37:25 +0200

    Added description

--- apps/frontend/pages/_app.js
+++ apps/frontend/pages/_app.js
@@ -2,6 +2,7 @@ import AppProvider from "../components/AppProvider"
 
 const MyApp = ({ Component, pageProps }) => {
   return (
+    // Contains both chakra & redux provider + layout
     <AppProvider>
       <Component {...pageProps} />
     </AppProvider>
